3a936d1b2d0c2fcf6314550bca506c0696663d59
Jiss Devra <jissdevra2003@gmail.com>
2025-12-03 23:14:11 +0530

    written product model

---
+++ server/src/models/Product.model.ts
@@ -0,0 +1,66 @@
+import mongoose , {Document, Schema,Types} from 'mongoose'
+
+export interface IProduct extends Document
+{
+    market_id:Types.ObjectId;
+    name:string
+
+  sku: string;                   // unique per market (stock keeping unit)
+  description?: string;
+
+  category?: string;
+  unit?: string;                 // e.g., "pcs", "kg", "box"
+  barcode?: string;              // barcode / EAN / UPC
+  
+  cost_price?: number;           // average/latest purchase cost (for valuation)
+  selling_price?: number;        // default retail price
+
+  stock_quantity?: number;       // simple global stock (V1). Use Stock collection for shop-level.
+
+  
+
+  image_urls?: string[];         // small gallery of image URLs
+
+  attributes?: Record<string, any>; // flexible key/value (size, color, etc.)
+
+  isActive: boolean;
+  created_by?: Types.ObjectId;
+  updated_by?: Types.ObjectId;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const productSchema=new Schema<IProduct>(
+{
+    market_id:{type:Schema.Types.ObjectId, ref:"Market",
+required:true,
+index:true
+
+    },
+    name:{type:String,required:true,trim:true},
+    sku:{type:String,required:true,trim:true},
+    description: { type: String, trim: true },
+
+     category: { type: String, trim: true },
+    unit: { type: String, trim: true, default: "pcs" },
+    barcode: { type: String, trim: true },
+
+    cost_price: { type: Number, default: 0, min: 0 },
+    selling_price: { type: Number, default: 0, min: 0 },
+
+    stock_quantity: { type: Number, default: 0 }, // global stock for V1
+
+     image_urls: [{ type: String, trim: true }],
+    attributes: { type: Schema.Types.Mixed },
+
+    isActive: { type: Boolean, default: true },
+
+    created_by: { type: Schema.Types.ObjectId, ref: "User" },
+    updated_by: { type: Schema.Types.ObjectId, ref: "User" }
+},
+{timestamps:true}
+
+)
+
+export const Product=mongoose.models.Product || mongoose.model<IProduct>("Product",productSchema)
