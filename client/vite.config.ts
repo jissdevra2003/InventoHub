6ad24bdbbc5a7881b5a0d0cc41b0f41953872d1b
Jiss Devra <jissdevra2003@gmail.com>
2026-04-27 13:42:53 +0530

    updated dashboard page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://inventohub-backend-20we.onrender.com',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       },
     },
